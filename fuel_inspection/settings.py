f01234a6cc47c3103272a7f6e7ba8ddf91a302d4
Kiarash <puddin@kali>
2024-06-29 04:00:31 +0330

    change detail page and add backend side to it

--- fuel_inspection/settings.py
+++ fuel_inspection/settings.py
@@ -8,8 +8,8 @@ SECRET_KEY = 'django-insecure-0($av-y$av&j3$8u*9*au*=+9!f&d3xq#m#y2@lew)^r(ezcwr
 
 DEBUG = True
 
-# ALLOWED_HOSTS = ['37.152.183.65', 'www.takkharidshop.ir', 'takkharidshop.ir']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['37.152.183.65', 'www.takkharidshop.ir', 'takkharidshop.ir', '127.0.0.1']
+# ALLOWED_HOSTS = []
 
 
 
