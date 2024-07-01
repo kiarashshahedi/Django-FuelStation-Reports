54ce240ae4c48f13112e0e35f6f2015007d51b0f
Kiarash Shahedi <puddin@kali>
2024-07-02 02:14:56 +0330

    change station file and merge all html file in station.html for making pages with javascript

--- inspections/views.py
+++ inspections/views.py
@@ -1,12 +1,12 @@
-# reports/views.py
-
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import FuelStation, Tank, Nozzle
 from django.template.loader import get_template
 from xhtml2pdf import pisa
-from django.conf import settings
-import os
+from django.db import transaction
+from django.urls import reverse
+
+
 
 def home(request):
     return render(request, 'home.html')
@@ -14,65 +14,84 @@ def home(request):
 def create_station(request):
     if request.method == 'POST':
         name = request.POST.get('name')
-        gasoline_tanks = int(request.POST.get('gasoline_tanks'))
-        gasoline_nozzles = int(request.POST.get('gasoline_nozzles'))
-        gas_nozzles = int(request.POST.get('gas_nozzles'))
-
-
-        gas_tanks = int(request.POST.get('gas_tanks'))
-        gasoline_beginning = float(request.POST.get('gasoline_beginning'))
-        gas_beginning = float(request.POST.get('gas_beginning'))
-        gasoline_received = float(request.POST.get('gasoline_received'))
-        gas_received = float(request.POST.get('gas_received'))
-        electronic_gasoline_sales = float(request.POST.get('electronic_gasoline_sales'))
-        electronic_gas_sales = float(request.POST.get('electronic_gas_sales'))
+        gasoline_tanks = int(request.POST.get('gasoline_tanks', 0))
+        gasoline_nozzles = int(request.POST.get('gasoline_nozzles', 0))
+        gas_nozzles = int(request.POST.get('gas_nozzles', 0))
+        gas_tanks = int(request.POST.get('gas_tanks', 0))
+        gasoline_beginning = float(request.POST.get('gasoline_beginning', 0))
+        gas_beginning = float(request.POST.get('gas_beginning', 0))
+        gasoline_received = float(request.POST.get('gasoline_received', 0))
+        gas_received = float(request.POST.get('gas_received', 0))
+        electronic_gasoline_sales = float(request.POST.get('electronic_gasoline_sales', 0))
+        electronic_gas_sales = float(request.POST.get('electronic_gas_sales', 0))
         start_date = request.POST.get('start_date')
         end_date = request.POST.get('end_date')
 
-        station = FuelStation.objects.create(
-            name=name,
-            gasoline_tanks=gasoline_tanks,
-            gas_tanks=gas_tanks,
-            gasoline_nozzles=gasoline_nozzles,
-            gas_nozzles=gas_nozzles,
-            gasoline_beginning=gasoline_beginning,
-            gas_beginning=gas_beginning,
-            gasoline_received=gasoline_received,
-            gas_received=gas_received,
-            electronic_gasoline_sales=electronic_gasoline_sales,
-            electronic_gas_sales=electronic_gas_sales,
-            start_date=start_date,
-            end_date=end_date
-        )
-        return redirect('add_tanks', station_id=station.id)
+        with transaction.atomic():
+            station = FuelStation.objects.create(
+                name=name,
+                gasoline_tanks=gasoline_tanks,
+                gas_tanks=gas_tanks,
+                gasoline_nozzles=gasoline_nozzles,
+                gas_nozzles=gas_nozzles,
+                gasoline_beginning=gasoline_beginning,
+                gas_beginning=gas_beginning,
+                gasoline_received=gasoline_received,
+                gas_received=gas_received,
+                electronic_gasoline_sales=electronic_gasoline_sales,
+                electronic_gas_sales=electronic_gas_sales,
+                start_date=start_date,
+                end_date=end_date
+            )
+
+            for i in range(gasoline_tanks):
+                amount = float(request.POST.get(f'gasoline_tank_{i}', 0))
+                Tank.objects.create(station=station, type='gasoline', amount=amount)
+
+            for i in range(gas_tanks):
+                amount = float(request.POST.get(f'gas_tank_{i}', 0))
+                Tank.objects.create(station=station, type='gas', amount=amount)
+
+            for i in range(gasoline_nozzles):
+                start_totalizer = float(request.POST.get(f'gasoline_nozzle_start_totalizer_{i}', 0))
+                end_totalizer = float(request.POST.get(f'gasoline_nozzle_end_totalizer_{i}', 0))
+                Nozzle.objects.create(station=station, type='gasoline', start_totalizer=start_totalizer, end_totalizer=end_totalizer)
+
+            for i in range(gas_nozzles):
+                start_totalizer = float(request.POST.get(f'gas_nozzle_start_totalizer_{i}', 0))
+                end_totalizer = float(request.POST.get(f'gas_nozzle_end_totalizer_{i}', 0))
+                Nozzle.objects.create(station=station, type='gas', start_totalizer=start_totalizer, end_totalizer=end_totalizer)
+
+        return redirect(reverse('station_detail', args=[station.id]))
+
     return render(request, 'create_station.html')
 
-def add_tanks(request, station_id):
-    station = FuelStation.objects.get(id=station_id)
-    if request.method == 'POST':
-        for i in range(station.gasoline_tanks):
-            amount = float(request.POST.get(f'gasoline_tank_{i}'))
-            Tank.objects.create(station=station, type='gasoline', amount=amount)
-        for i in range(station.gas_tanks):
-            amount = float(request.POST.get(f'gas_tank_{i}'))
-            Tank.objects.create(station=station, type='gas', amount=amount)
-        return redirect('add_nozzles', station_id=station.id)
-    return render(request, 'add_tanks.html', {'station': station})
+# def add_tanks(request, station_id):
+#     station = FuelStation.objects.get(id=station_id)
+#     if request.method == 'POST':
+#         for i in range(station.gasoline_tanks):
+#             amount = float(request.POST.get(f'gasoline_tank_{i}'))
+#             Tank.objects.create(station=station, type='gasoline', amount=amount)
+#         for i in range(station.gas_tanks):
+#             amount = float(request.POST.get(f'gas_tank_{i}'))
+#             Tank.objects.create(station=station, type='gas', amount=amount)
+#         return redirect('add_nozzles', station_id=station.id)
+#     return render(request, 'add_tanks.html', {'station': station})
 
-def add_nozzles(request, station_id):
-    station = FuelStation.objects.get(id=station_id)
+# def add_nozzles(request, station_id):
+#     station = FuelStation.objects.get(id=station_id)
         
-    if request.method == 'POST':
-        for i in range(station.gasoline_nozzles):
-            start_totalizer = float(request.POST.get(f'gasoline_nozzle_start_totalizer_{i}'))
-            end_totalizer = float(request.POST.get(f'gasoline_nozzle_end_totalizer_{i}'))
-            Nozzle.objects.create(station=station, type='gasoline', start_totalizer=start_totalizer, end_totalizer=end_totalizer)
-        for i in range(station.gas_nozzles):
-            start_totalizer = float(request.POST.get(f'gas_nozzle_start_totalizer_{i}'))
-            end_totalizer = float(request.POST.get(f'gas_nozzle_end_totalizer_{i}'))
-            Nozzle.objects.create(station=station, type='gas', start_totalizer=start_totalizer, end_totalizer=end_totalizer)
-        return redirect('station_detail', station_id=station.id)
-    return render(request, 'add_nozzles.html', {'station': station})
+#     if request.method == 'POST':
+#         for i in range(station.gasoline_nozzles):
+#             start_totalizer = float(request.POST.get(f'gasoline_nozzle_start_totalizer_{i}'))
+#             end_totalizer = float(request.POST.get(f'gasoline_nozzle_end_totalizer_{i}'))
+#             Nozzle.objects.create(station=station, type='gasoline', start_totalizer=start_totalizer, end_totalizer=end_totalizer)
+#         for i in range(station.gas_nozzles):
+#             start_totalizer = float(request.POST.get(f'gas_nozzle_start_totalizer_{i}'))
+#             end_totalizer = float(request.POST.get(f'gas_nozzle_end_totalizer_{i}'))
+#             Nozzle.objects.create(station=station, type='gas', start_totalizer=start_totalizer, end_totalizer=end_totalizer)
+#         return redirect('station_detail', station_id=station.id)
+#     return render(request, 'add_nozzles.html', {'station': station})
 
 def station_detail(request, station_id):
     station = FuelStation.objects.get(id=station_id)
@@ -129,7 +148,6 @@ def station_detail(request, station_id):
         'qire_mojaz': qire_mojaz,
         'electronic_mechanical_discrepancy': electronic_mechanical_discrepancy,
         'electronic_mechanical_discrepancy_gas': electronic_mechanical_discrepancy_gas,
-        
     }
 
     return render(request, 'station_detail.html', context)
